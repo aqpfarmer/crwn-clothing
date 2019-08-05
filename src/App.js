0229ae3a1c82b7a11cc6a635b1e9aea7acc5e50f
Frikkin Genius <aqpfarmer@gmail.com>
2019-08-05 11:16:46 -0400

    saving userauth

--- src/App.js
+++ src/App.js
@@ -21,8 +21,14 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          console.log('Got an authorized user...')
+          console.log(snapShot);
+        });
+      }
     });
   }
 
