d2a19f13c1e804309a07306e0711915db864dbd6
Frikking Genius <aqpfarmer@gmail.com>
2019-08-06 18:14:25 -0400

    created cart-items compoennt and connected component to dropdown

--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -1,16 +1,16 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id = cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
 
     if (existingCartItem) {
         console.log('existing is true')
         return cartItems.map(cartItem =>
-            cartItem.id = cartItemToAdd.id 
+            cartItem.id === cartItemToAdd.id 
             ? {...cartItem, quantity: cartItem.quantity + 1}
             : cartItem 
-            );
+        );
     }
 
-    return [...cartItems, { cartItemToAdd, quantity:1} ];
+    return [...cartItems, { ...cartItemToAdd, quantity:1} ];
 };
