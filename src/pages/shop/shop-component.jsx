e580346c4f604710291c1f2dcb5153d2b30e2380
grassman <aqpfarmer@gmail.com>
2019-08-14 19:08:59 -0400

    Enabled collections and updated reducers so that we can click on a category and its routed to the right things

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import CollectionsOverView from '../../components/collections-overview/collections-overview-component';
-import CategoryPage from '../../pages/category/category-component';
+import CollectionPage from '../../pages/collection/collection-component';
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverView} />
-        <Route path={`${match.path}/:category`} component={CategoryPage} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
