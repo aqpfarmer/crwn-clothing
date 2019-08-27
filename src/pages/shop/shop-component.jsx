5efa2f0a5eee86e5b71a082e5aed1063e3e4116e
Chris <chris@mrchips.xyz>
2019-08-27 15:59:45 -0400

    added fidget spinner HOC

--- src/pages/shop/shop-component.jsx
+++ src/pages/shop/shop-component.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import WithSpinner from '../../components/with-spinner/with-spinner-component';
+
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview-component';
 import CollectionPage from '../collection/collection-component';
 
 import { firestore, convetCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-import  { dispatch } from 'rxjs/internal/observable/pairs';
+//import  { dispatch } from 'rxjs/internal/observable/pairs';
 
 import { updateCollections } from '../../redux/shop/shop-actions';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
   unsubscribeFromSnapshot = null;
   
   componentDidMount() {
@@ -21,16 +29,21 @@ class ShopPage extends React.Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convetCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   };
 
   render() {
-
     const { match } = this.props;
+    const { loading } = this.state;
     return  (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        <Route exact path={`${match.path}`} render={props => (
+           <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+        )} />
+        <Route path={`${match.path}/:collectionId`} render={props => (
+            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+        )} />
       </div>
     );
   };
